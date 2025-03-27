00612172886fa0decf3054429f6f3197dbb42566
Hasatria <satriasrya30@gmail.com>
2025-03-27 23:40:47 +0700

    Add files via upload

---
+++ CRUD/products/deleteproducts.php
@@ -0,0 +1,11 @@
+
+<?php
+include '../config.php';
+$id = intval($_GET['id']);
+
+$stmt = $conn->prepare("DELETE FROM products WHERE id=?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+header("Location: tabelproducts.php");
+?>
